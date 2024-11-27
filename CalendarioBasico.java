2d9da007db5c3cf91c24c0f2fba86541f1abdd59
OnicarN <ebaudani@gmail.com>
2024-11-27 13:08:11 +0100

    ggug

--- CalendarioBasico.java
+++ CalendarioBasico.java
@@ -10,8 +10,22 @@ public class CalendarioBasico{
       ano = 1;
   }
   public String obteneFecha() {
-      return dia +"-"+mes+"-"+ano;
+      String elDia = ""+dia;
+      String esteMes = ""+mes;
+      String esteAno = ""+ano;
+      
+      if(dia < 10){
+          elDia = "0"+dia;
       }
+      if (mes < 10){
+          esteMes = "0"+mes;
+      }
+      if (ano <10){
+          esteAno = "0"+ano;
+      }return elDia +"-"+esteMes+"-"+esteAno;
+      
+    
+}
   public void fijarFecha(int nuevoDia, int nuevoMes, int nuevoAno){
       nuevoDia = nuevoDia;
       nuevoMes = nuevoMes;
@@ -21,6 +35,25 @@ public class CalendarioBasico{
       mes = nuevoMes;
       ano = nuevoAno;
   }
+  
+  public void avanzarFecha(){
+      dia = dia + 1;
+      
+      if(dia > 30){
+          dia = 1;
+          mes = mes + 1;
+      }
+      if (mes > 12){
+          mes = 1;
+          ano = ano + 1;
+      }
+      if (ano > 99){
+          dia = 1;
+          mes = 1;
+          ano = 1;
+      }
+  }
+  
   
 
   
