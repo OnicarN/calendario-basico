6daf005d880973ffd4fbbd2e21408fb76978a2fc
OnicarN <ebaudani@gmail.com>
2024-11-27 13:03:06 +0100

    visto

--- CalendarioBasico.java
+++ CalendarioBasico.java
@@ -10,22 +10,8 @@ public class CalendarioBasico{
       ano = 1;
   }
   public String obteneFecha() {
-      String elDia = ""+dia;
-      String esteMes = ""+mes;
-      String esteAno = ""+ano;
-      
-      if(dia < 10){
-          elDia = "0"+dia;
+      return dia +"-"+mes+"-"+ano;
       }
-      if (mes < 10){
-          esteMes = "0"+mes;
-      }
-      if (ano <10){
-          esteAno = "0"+ano;
-      }return elDia +"-"+esteMes+"-"+esteAno;
-      
-    
-}
   public void fijarFecha(int nuevoDia, int nuevoMes, int nuevoAno){
       nuevoDia = nuevoDia;
       nuevoMes = nuevoMes;
